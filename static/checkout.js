9faa1ec5306d41f7cfeb8ca05290858d588bbdd4
Mau5trakt <kubzalemany@hotmail.com>
2024-01-03 01:18:32 -0600

    feat: created function to change the intem quantity from a input and re-render the prices

--- static/checkout.js
+++ static/checkout.js
@@ -77,12 +77,15 @@ function cartRenderize(){
     price.classList.add("text-body-secondary")
     price.textContent = `$${(articulo.pventa * articulo.cantidad).toFixed(2)}`
 
-    cuenta += articulo.pventa
+    cuenta += articulo.pventa * articulo.cantidad
 
     price.id = "priceItem"
     input.type ="text"
     input.value = articulo.cantidad
-    input.addEventListener("input", changeQty)
+    input.addEventListener("input", function () {
+      changeQty(this)
+    })
+    input.id = "inputQty"
 
     //making the element
     small.insertAdjacentElement("beforeend", eliminar)
@@ -112,9 +115,101 @@ function cartRenderize(){
 total.textContent = `$ ${cuenta.toFixed(2).toString()}`
 }
 
-function changeQty(){
-  console.log("it's going to be modified")
+function changeQty(element) {
+    let alertInvalid = document.createElement("div");
+    alertInvalid.classList.add("alert", "alert-danger");
+    alertInvalid.role = "alert";
+    alertInvalid.textContent = "Cantidad invalida";
+    alertInvalid.id = "error-alert";
+
+    let alertQty = document.createElement("div")
+    alertQty.classList.add("alert", "alert-warning")
+    alertQty.role = "alert"
+    alertQty.textContent = "Cantidad no disponible"
+    alertQty.id = "invalid-qty";
+
+    let divItem = element.parentNode;
+    let cantidad = element.value;
+
+    // Use a regular expression to check if the entire input is numeric or check if the qty is 0
+    if (!/^\d+$/.test(cantidad) || cantidad === "0") {
+        element.classList.add("invalid-input");
+
+        // Check if there is an error div, if not, insert it
+        if (!divItem.querySelector("#error-alert")) {
+            divItem.insertAdjacentElement("afterbegin", alertInvalid);
+        }
+    } else {
+        element.classList.remove("invalid-input");
+
+        // Remove the error div if it exists
+        let errorAlert = divItem.querySelector("#error-alert");
+        if (errorAlert) {
+            divItem.removeChild(errorAlert);
+        }
+
+        let product = element.parentNode.parentNode
+        let name = product.querySelector("h6").textContent
+        let qty = element.value;
+        let carrito = JSON.parse(localStorage.getItem('Carrito')) || [];
+
+        let elementIndex = carrito.findIndex(p => p.elemento === name)
+        if (elementIndex !== -1)
+        {
+
+            if (qty > carrito[elementIndex].disponibles)
+            {
+                    element.classList.add("invalid-qty");
+
+                    // Check if there is an invalid-qty div, if not, insert it
+                    if (!divItem.querySelector("#invalid-qty"))
+                    {
+                        divItem.insertAdjacentElement("afterbegin", alertQty);
+                    }
+            }
+            else
+            {
+                // Remove the invalid-qty class and div if it exists
+                element.classList.remove("invalid-qty");
+                let invalidQtyAlert = product.querySelector("#invalid-qty");
+                if (invalidQtyAlert) {
+                    divItem.removeChild(invalidQtyAlert);
+                }
+
+                let totalArt = (qty * carrito[elementIndex].pventa).toFixed(2)
+                let producto = element.parentNode.parentNode
+                let priceHtml = producto.querySelector("span")
+                priceHtml.textContent = `$${totalArt.toString()}`
+
+                let products = document.querySelectorAll("#product")
+
+                let cuenta = 0;
+                //For here i gotta modify to add the delivery field and add it to the count
+                // Also I've to convert the delivery value
+                // AND CONVERT EVERYTHING TO CORDOBAS
+                // AND SHOW EVERY SINGLE ITEM ALSO IN CORDOBAS
+                for (let p of products)
+                {
+                    let priceRow = p.querySelector("span")
+                    //cuenta += priceRow.textContent
+                    console.log(priceRow.textContent)
+                    let priceConverted = parseFloat(priceRow.textContent.replace("$", ""))
+
+                    cuenta += priceConverted
+
+
+                }
+                console.log(cuenta)
+                document.querySelector("#total").textContent = cuenta.toFixed(2)
+
+
+            }
+
+        }
+    }
 }
+
+
 
 function deleteItem(element){
   let div = (element.parentNode.parentNode)
